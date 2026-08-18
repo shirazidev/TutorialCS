d3ddda5d45b18f7cf48e0267d2e7b619de6b690c
AmirHossein Shirazi <ahshirazi.dev@gmail.com>
2026-08-19 00:07:06 +0330

    Add property to class in Program.cs

--- TutorialEquality/Program.cs
+++ TutorialEquality/Program.cs
@@ -1,26 +1,26 @@
 ﻿var myClass = new MyClass { NumericValue = 1, StringValue = "Hello Class" };
-var myClass1 = new MyClass{ NumericValue = 1, StringValue = "Hello Class" };
+var myClass1 = new MyClass { NumericValue = 1, StringValue = "Hello Class" };
 
 Console.WriteLine("check if class equals to same class?");
 Console.WriteLine(myClass == myClass1);
 Console.WriteLine(myClass.Equals(myClass1));
-Console.WriteLine(object.Equals(myClass, myClass1));
+Console.WriteLine(Equals(myClass, myClass1));
 
 var myStruct = new MyStruct { NumericValue = 1, StringValue = "Hello Struct" };
-var myStruct1 = new MyStruct{ NumericValue = 1, StringValue = "Hello Struct" };
+var myStruct1 = new MyStruct { NumericValue = 1, StringValue = "Hello Struct" };
 Console.WriteLine("check if struct equals to struct?");
 // Console.WriteLine(myStruct == myStruct1);
 Console.WriteLine(myStruct.Equals(myStruct1));
-Console.WriteLine(object.Equals(myStruct, myStruct1));
+Console.WriteLine(Equals(myStruct, myStruct1));
 
 
 var myClassE = new MyClassWithEquality { NumericValue = 1, StringValue = "Hello Class" };
-var myClassE1 = new MyClassWithEquality{ NumericValue = 1, StringValue = "Hello Class" };
+var myClassE1 = new MyClassWithEquality { NumericValue = 1, StringValue = "Hello Class" };
 
 Console.WriteLine("check if class equals to same class?");
 Console.WriteLine(myClassE == myClassE1);
 Console.WriteLine(myClassE.Equals(myClassE1));
-Console.WriteLine(object.Equals(myClassE, myClassE1));
+Console.WriteLine(Equals(myClassE, myClassE1));
 
 public class MyClass
 {
@@ -42,7 +42,7 @@ class MyClassWithEquality
 
     public override bool Equals(object obj)
     {
-        if ( obj == null || GetType() != obj.GetType())
+        if (obj == null || GetType() != obj.GetType())
         {
             return false;
         }
@@ -50,7 +50,7 @@ class MyClassWithEquality
         var other = (MyClassWithEquality)obj;
         return NumericValue == other.NumericValue && StringValue == other.StringValue;
     }
-    
+
     public override int GetHashCode()
     {
         return NumericValue.GetHashCode() ^ StringValue.GetHashCode();
@@ -60,9 +60,9 @@ class MyClassWithEquality
     {
         return left.Equals(right);
     }
+
     public static bool operator !=(MyClassWithEquality left, MyClassWithEquality right)
     {
         return !left.Equals(right);
     }
-    
 }
